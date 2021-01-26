ae24dbeb9890fb2aac7e1c697eedd63b0c96c62e
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-26 21:49:52 +0000

    10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,7 @@ def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
         return []
     else:
+        res=[]
         for i in range(len(my_list)):
             if my_list[i] % 2 == 0:
                 res[i] = True
