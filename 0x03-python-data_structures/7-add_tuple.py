62c3116b59d5c308faa6c0eb924ca8dc9a8f9994
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-26 21:23:45 +0000

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return (0, 0)
+    elif len(tuple_a) == 0:
         sum1 = tuple_b[0]
         sum2 = tuple_b[1]
     elif len(tuple_b) == 0:
