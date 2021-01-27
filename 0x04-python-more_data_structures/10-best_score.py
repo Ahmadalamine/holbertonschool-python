81142efc7479fa349a4fde45678171427a549f67
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-27 20:02:27 +0000

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        items=list(a_dictionary.values)
+        items=list(a_dictionary.values())
         items.sort(reverse = True)
         return items[0]
