55f23f4641d340c2dfa225c65e05e5e75504d28c
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-27 18:21:48 +0000

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,4 +4,4 @@ def search_replace(my_list, search, replace):
         for i in range(len(my_list)):
             if my_list[i] == search:
                     newlist[i] = replace
-        return newlist
+    return newlist
