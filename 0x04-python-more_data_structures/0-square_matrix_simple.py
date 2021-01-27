f5348b7ae5503a91b2334a034f3ce4cec7bdf79b
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-27 17:52:06 +0000

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,6 @@ def square_matrix_simple(matrix=[]):
     if len(matrix) == 0:
         return matrix
     else:
-        newm = map(lambda x: x ** 2, matrix)
-        return list(newm)
+        newm = map(lambda x: x * x, matrix)
+        m=list(newm)
+        return m
