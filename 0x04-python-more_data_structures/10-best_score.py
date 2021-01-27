66ebbcdb233a26d364d6e6d90c0048462d32341e
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-27 20:10:43 +0000

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
         return None
     else:
         keyy =''
-        max = a_dictionary.values[0]
+        max = a_dictionary.values()[0]
         for key, value in a_dictionary.items():
             if value > max:
                 max = value
