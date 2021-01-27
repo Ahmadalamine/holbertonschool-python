c541ec87fd7381e963cef520ce4e11f017c80451
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-27 18:01:40 +0000

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if len(matrix) == 0:
-        return matrix
-    else:
-        newmatrix = matrix.copy()
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                newmatrix[i][j] = matrix[i][j] * matrix[i][j]
-        return newmatrix
+    newmatrix = matrix.copy()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            newmatrix[i][j] = matrix[i][j] * matrix[i][j]
+    return newmatrix
