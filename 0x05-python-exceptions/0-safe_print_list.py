c73d2ab178784e158752edfb3603f356fcdd7419
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-29 01:50:55 +0000

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,6 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list[:x]:
             print(my_list[i], end="")
     except IndexError:
-            
+            return i
     return i
 
