0d3d5494df60be83500087eb2d8fe043e84e6318
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-01-31 20:33:19 +0000

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for i in str:
         if 96 < ord(i) < 121:
             i = ord(i) - 32
-        res = res + i
-    print("{}".format(str))
+        res += i
+    print("{}".format(res))
