a57c47a3c2f8e0c357812abb03c84961f6a65159
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-02-02 21:14:07 +0000

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,10 +3,14 @@
 
 
 class Square:
-    """this classs contains the constructor and on
-    private attribute"""
+"""this classs contains the constructor and on
+private attribute"""
+
+
     def __init__(self, size=0):
         """this functions is the const"""    
+
+
         try:
             __size = size
         except TypeError:
