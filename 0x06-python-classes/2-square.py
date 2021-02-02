52b8f2852af2d6b11a56e8d65b1cb707d9f1a7cd
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-02-02 21:20:55 +0000

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """this classs contains the constructor and on private attribute."""
     def __init__(self, size=0):
-        __size = size
+        self.__size = size
         if type(self.__size) != int:
             raise("size must be an integer")
         if self.__size < 0
