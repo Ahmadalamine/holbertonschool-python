2da33a9aa66e39391ad7cda3ecdea24ed9745915
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-02-03 20:54:16 +0000

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,20 +11,15 @@ class Square:
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
-
     @property
-
     def size(self):
         return self.__size
-
     @size.setter
-
     def size(self, value):
         self.__size = value
         if type(self.__size) is not int:    
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
-
     def area(self):
         return self.__size ** 2
