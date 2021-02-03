640df6820e50cb93e1f5529c35c927eb5562b1fa
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-02-03 21:09:02 +0000

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,14 +4,13 @@
 
 class Square:
     """this class contains the constructor and on private attribute."""
-
-    def __init__(self, size=0):
+    
+def __init__(self, size=0):
         self.__size = size
         if type(self.__size) is not int:
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
-
 
     @property
     def size(self):
