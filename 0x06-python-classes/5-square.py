86f5d696b8e0a9fb4025408788f5dac332766c2a
Ahmadalamine <ahmad.am.2519@gmail.com>
2021-02-03 21:16:51 +0000

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""this module contains one class."""
+
+
+class Square:
+    """this class contains the constructor and on private attribute."""
+    
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        else:
+            for i in range(__size):
+                for j in (__size):
+                    print("#", end="")
+                print()
