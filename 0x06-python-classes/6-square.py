e317b4397c1dcb659808f4022501cd52b994b029
ahmad <ahmad.am.2519@gmail.com>
2021-02-14 12:13:52 -0800

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,11 @@ class Square:
             print()
             return
         else:
+            for z in range(self.__position[1]):
+                print()
             for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
