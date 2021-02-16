67e0b00e1e389c179180e610fd9f534e3d14b026
ahmad <ahmad.am.2519@gmail.com>
2021-02-16 12:58:43 -0800

    A

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -5,13 +5,12 @@
 def canUnlockAll(boxes):
     """d"""
 
-    c = 0
     keys = boxes[0].copy()
     for i in keys:
-        if not boxes[i]:
-            c += 1
         for j in boxes[i]:
             if j not in keys:
                 keys.append(j)
-    res = len(keys) + c
-    return len(boxes) == res
+    for i in range(len(boxes)):
+        if i not in keys:
+            return False
+    return True
