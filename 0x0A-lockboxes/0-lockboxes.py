75e3a95e7ff8f94a866d3e86b2713aedfdac954e
ahmad <ahmad.am.2519@gmail.com>
2021-02-16 13:09:15 -0800

    A

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -7,10 +7,17 @@ def canUnlockAll(boxes):
 
     keys = boxes[0].copy()
     for i in keys:
-        for j in boxes[i]:
-            if j not in keys:
-                keys.append(j)
+        try:
+            for j in boxes[i]:
+                if j not in keys:
+                    keys.append(j)
+        except IndexError:
+            return False
     for i in range(1, len(boxes)):
         if i not in keys:
             return False
     return True
+
+
+boxes = [ [7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3], [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7], [4, 2, 9, 6, 6, 5, 5], ]
+print(canUnlockAll(boxes))
