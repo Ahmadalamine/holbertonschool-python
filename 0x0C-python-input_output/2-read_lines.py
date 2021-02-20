8f781412081c9d944bcb6abb9ea6c9dfcbf1acb0
ahmad <ahmad.am.2519@gmail.com>
2021-02-20 10:37:08 -0800

    2

--- 0x0C-python-input_output/2-read_lines.py
+++ 0x0C-python-input_output/2-read_lines.py
@@ -6,6 +6,10 @@ def read_lines(filename="", nb_lines=0):
     """doc"""
     line = ""
     with open(filename) as f:
+        if nb_lines <= 0 or nb_lines >= len(f.readlines()):
+            en_file = f.read()
+            print(en_file, end="")
+            return
         while nb_lines:
             line = f.readline()
             print(line, end="")
