7692c42f2fd736ba6a57c233f63051042aec9f63
ahmad <ahmad.am.2519@gmail.com>
2021-02-20 10:27:10 -0800

    1

--- 0x0C-python-input_output/1-number_of_lines.py
+++ 0x0C-python-input_output/1-number_of_lines.py
@@ -6,5 +6,5 @@ def number_of_lines(filename=""):
     """doc"""
     nb_of_line = 0
     with open(filename, 'r') as f:
-        f.readline()
-        nb_of_line += 1
+        for line in f:
+            nb_of_line += 1
